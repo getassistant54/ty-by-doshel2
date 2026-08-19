59d52a96ae912b48c04ee9e203adf85e47824151
Lena <lena@example.com>
2026-08-19 14:33:21 +0700

    Fix goal label property in goal-screen.js

--- js/components/goal-screen.js
+++ js/components/goal-screen.js
@@ -1,7 +1,7 @@
 import { renderShell, renderTopbar } from './layout.js';
 import { GOALS } from '../data/goals.js';
 
-export function renderGoalScreen(selectedGoalId = null) {
+export function renderGoalScreen({ selectedGoalId = null } = {}) {
   return renderShell(`
     ${renderTopbar(10)}
     <div class="flex-1 flex flex-col pt-1">
@@ -13,7 +13,7 @@ export function renderGoalScreen(selectedGoalId = null) {
         ${GOALS.map((goal) => `
           <button class="option-card goal-option-btn ${selectedGoalId === goal.id ? 'option-selected' : ''}" data-goal-id="${goal.id}" aria-pressed="${selectedGoalId === goal.id}">
             <span class="option-icon"><i data-lucide="${goal.icon}" class="w-4 h-4"></i></span>
-            <span class="option-copy">${goal.title}</span>
+            <span class="option-copy">${goal.label || goal.title}</span>
           </button>
         `).join('')}
       </div>
